fae7d11543db42d6b420e137b7615446cdc02b6a
ikhmal2 <ax.ikhmal.145713@gmail.com>
2022-09-02 01:46:04 +0800

    added card skeleton and css

--- src/components/projects-homepage.vue
+++ src/components/projects-homepage.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        
+    <div class="project-card">
+        <img src="../assets/homepage.png" alt="portfolio-homepage">
     </div>
 </template>
 
@@ -9,5 +9,16 @@
 </script>
 
 <style scoped>
+    .project-card {
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+    }
 
+    .project-card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+
+    .project-card img {
+        
+    }
 </style>
