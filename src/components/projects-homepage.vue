5a9c707a730bc8dc498f5eda8df74dc3e7709a2b
ikhmal2 <ax.ikhmal.145713@gmail.com>
2022-09-03 02:28:40 +0800

    trying to read data from json

--- src/components/projects-homepage.vue
+++ src/components/projects-homepage.vue
@@ -1,29 +1,20 @@
 <template>
     <span class="project-title">Projects</span>
     <ul class="projects-list">
-        <li v-for="(project, index) in projects" :key="index" class="project-item">
+        <li class="project-item" :for="(project, index) in projectData" :key="index">
             <div class="project-card">
-                <img v-bind:attr="src=project.src, alt=project.title">
-            </div>
-        </li>
-        <li class="project-item">
-            <div class="project-card">
-                <img src="../assets/homepage.png" alt="">
+                <h2>{{ project.title }}</h2>
             </div>
         </li>
     </ul>
 </template>
 
 <script>
+    import projects from '../assets/json/projects.json'
     export default {
         data() {
             return { 
-                projects: [
-                    {src: "../assets/homepage.png", title: "My Portfolio", desc: "This portfolio is made with Vue.js and Ikhmal used Figma to design it. He planned to use Bootstrap for his CSS but there're not a lot of components in here so he decided to just use vanilla css instead", link: "#"},
-                    {src: "../assets/ambankspot.png", title: "AmbankSpot Malaysia", desc: "This website is made with Wordpress with a custom theme and custom plugin installed for it. He also use a popular web builder plugin called Elementor and used Jquery as his goto library. (yea i know its old but its easy for beginners right?)", link: 'https://ambankspot.com/'},
-                    {src: "../assets/honda.png", title: "Honda Malaysia", desc: "The website is  made with Laravel eventhough his seniors made it, Ikhmal still updates the site when there're new promotions or campaign comes out", link: 'https://www.honda.com.my/'},
-                    {src: "../assets/pdh.png", title: "Pavilion Damansara Heights", desc: "It is also made with Wordpress and yes, he did used Jquery for it.  This website uses plugins such as Visual Composer (web builder), WPForm and Revolution Slider", link: 'https://paviliondamansaraheights.com/'},
-                ]
+                projectData: projects,
             }
         }
     }
