b6688119b2ce41ee056b173a045022b5ac692da5
ikhmal2 <ax.ikhmal.145713@gmail.com>
2022-09-26 20:19:56 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
-
+# abort on errors
 set -e
-
+# build
 npm run build
-
+# navigate into the build output directory
 cd dist
-
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 git init
 git add -A
-git commit -m 'New Deployment'
-git push -f https://github.com/ikhmal2/personalporfolio.git master:gh-pages
-
+git commit -m 'deploy'
+git push -f git@github.com:ikhmal2/personalporfolio.git master:gh-pages
 cd -
