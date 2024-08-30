5fb64b7e736697cf3b3e2afa0e33b021831f665d
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2024-08-30 16:27:39 +0800

    completed experience and added CI/CD config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	basePath: "/personalporfolio",
+	output: "export",
+	reactStrictMode: true
+};
 
 export default nextConfig;
---
+++ src/components/experience.tsx
@@ -0,0 +1,181 @@
+export default function Experience() {
+	return (
+		<section
+			id="experience"
+			className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+			aria-label="Work experience"
+		>
+			<div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+				<h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+					Experience
+				</h2>
+			</div>
+			<div>
+				<ol className="group/list">
+					<li className="mb-12">
+						<div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+							<div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+							<header
+								className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+								aria-label="May 2024 to Present"
+							>
+								May 2023 — Present
+							</header>
+							<div className="z-10 sm:col-span-6">
+								<h3 className="font-medium leading-snug text-slate-200">
+									<div>
+										<a
+											className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+											href="https://silverwind.digital/"
+											target="_blank"
+											rel="noreferrer noopener"
+											aria-label="Front-end Web Engineer, Accessibility at Silverwind (opens in a new tab)"
+										>
+											<span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block" />
+											<span>
+												Front-end Web Engineer ·{/* */}{" "}
+												<span className="inline-block">
+													Silverwind
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 20 20"
+														fill="currentColor"
+														className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+														aria-hidden="true"
+													>
+														<path
+															fillRule="evenodd"
+															d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+															clipRule="evenodd"
+														/>
+													</svg>
+												</span>
+											</span>
+										</a>
+									</div>
+								</h3>
+								<p className="mt-2 text-sm leading-normal">
+									Build and maintain critical components used to construct our main client AIA Malaysia frontend. Work closely with cross-functional teams, including developers, product owners, and business team, to implement and advocate for best practices catering to our users needs.
+								</p>
+								<ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
+									<li className="mr-1.5 mt-2">
+										<div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+											JavaScript
+										</div>
+									</li>
+									<li className="mr-1.5 mt-2">
+										<div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+											Vue
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</li>
+					<li className="mb-12">
+						<div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+							<div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+							<header
+								className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+								aria-label="Jan 2022 to May 2023"
+							>
+								Jan 2022 - May 2023
+							</header>
+							<div className="z-10 sm:col-span-6">
+								<h3 className="font-medium leading-snug text-slate-200">
+									<div>
+										<a
+											className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+											href="https://www.dentsucreative.com/location/malaysia"
+											target="_blank"
+											rel="noreferrer noopener"
+											aria-label="Front End Web Developer at Dentsu International (opens in a new tab)"
+										>
+											<span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block" />
+											<span>
+												Front End Web Developer ·{/* */}{" "}
+												<span className="inline-block">
+													Dentsu International
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 20 20"
+														fill="currentColor"
+														className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+														aria-hidden="true"
+													>
+														<path
+															fillRule="evenodd"
+															d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+															clipRule="evenodd"
+														/>
+													</svg>
+												</span>
+											</span>
+										</a>
+									</div>
+								</h3>
+								<p className="mt-2 text-sm leading-normal">
+									Build, style, and ship high-quality websites,
+									and digital experiences for a diverse array of
+									projects for clients including Honda Malaysia, Ambank, Pavilion, Vinda Deluxe and more.
+								</p>
+								<ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
+									<li className="mr-1.5 mt-2">
+										<div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+											JavaScript
+										</div>
+									</li>
+									<li className="mr-1.5 mt-2">
+										<div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+											HTML &amp; CSS
+										</div>
+									</li>
+									<li className="mr-1.5 mt-2">
+										<div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+											WordPress
+										</div>
+									</li>
+									<li className="mr-1.5 mt-2">
+										<div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+											PHP
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</li>
+				</ol>
+				<div className="mt-12">
+					<a
+						className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base"
+						href="/resume.pdf"
+						target="_blank"
+						rel="noreferrer noopener"
+						aria-label="View Full Résumé (opens in a new tab)"
+					>
+						<span>
+							View Full{/* */}{" "}
+							<span className="inline-block">
+								Résumé
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 20 20"
+									fill="currentColor"
+									className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+									aria-hidden="true"
+								>
+									<path
+										fillRule="evenodd"
+										d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+										clipRule="evenodd"
+									/>
+								</svg>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+		</section>
+
+	)
+};
